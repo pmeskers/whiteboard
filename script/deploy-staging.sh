b7c1fea4b7427e8edd3febaf70d63dc35bdb4b6d
Max Gordon <gordonmaxc@gmail.com>
2015-08-17 14:49:08 -0700

    Revert to legacy Travis infrastructure This allows use of sudo, which is needed to install cf-cli
    Signed-off-by: Jenny Chou <j.3h15u@gmail.com>

--- script/deploy-staging.sh
+++ script/deploy-staging.sh
@@ -2,7 +2,7 @@
 
 set -ev
 
-if [[ "$TRAVIS_BRANCH" == "master" ]] && [[ "$TEST" == "ruby" ]]; then
+if [[ "$TRAVIS_BRANCH" == "master" ]]; then
   wget -q -O cf-cli.deb https://cli.run.pivotal.io/stable?release=debian64
   sudo dpkg -i cf-cli.deb
   cf login -u $CF_USERNAME -p $CF_PASSWORD -o pivotallabs -s whiteboard -a https://api.run.pivotal.io
