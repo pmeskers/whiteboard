5cc642e07c2821fceedb1f65c6e2eb098a00d4ef
Max Gordon <gordonmaxc@gmail.com>
2015-08-17 15:41:14 -0700

    Remove superfluous sudo
    Signed-off-by: Jenny Chou <j.3h15u@gmail.com>

--- script/deploy-staging.sh
+++ script/deploy-staging.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
 set -ev
-
 if [[ "$TRAVIS_BRANCH" == "master" ]]; then
   wget -q -O cf-cli.deb https://cli.run.pivotal.io/stable?release=debian64
-  sudo dpkg -i cf-cli.deb
+  dpkg -i cf-cli.deb
   cf login -u $CF_USERNAME -p $CF_PASSWORD -o pivotallabs -s whiteboard -a https://api.run.pivotal.io
   git checkout .
   bundle exec rake SPACE=whiteboard cf:deploy:staging
