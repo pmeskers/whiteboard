8e37197cfa1d320871b205a5f29136bb7d62a0b5
Max Gordon <gordonmaxc@gmail.com>
2015-08-17 16:33:25 -0700

    Travis Debugging
    Signed-off-by: Sid Ho <sid@sidho.me>

--- script/deploy-staging.sh
+++ script/deploy-staging.sh
@@ -2,15 +2,11 @@
 
 set -ev
 if [[ "$TRAVIS_BRANCH" == "master" ]]; then
-  echo 'W($&^%(!*#&$(!*^#%$(&!#@^'
   bundler --version
-  echo 'W($&^%(!*#&$(!*^#%$(&!#@^'
   wget -q -O cf-cli.deb https://cli.run.pivotal.io/stable?release=debian64
   sudo dpkg -i cf-cli.deb
   cf login -u $CF_USERNAME -p $CF_PASSWORD -o pivotallabs -s whiteboard -a https://api.run.pivotal.io
-  echo 'W($&^%(!*#&$(!*^#%$(&!#@^'
   git diff
-  echo 'W($&^%(!*#&$(!*^#%$(&!#@^'
   git checkout .
   bundle exec rake SPACE=whiteboard cf:deploy:staging
 fi
