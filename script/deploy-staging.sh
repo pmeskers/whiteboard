5176be648cb6b2c7c326c56421768180bb55f61a
Max Gordon <gordonmaxc@gmail.com>
2015-08-17 16:52:21 -0700

    Travis Debugging
    Signed-off-by: Sid Ho <sid@sidho.me>

--- script/deploy-staging.sh
+++ script/deploy-staging.sh
@@ -6,6 +6,7 @@ if [[ "$TRAVIS_BRANCH" == "master" ]]; then
   wget -q -O cf-cli.deb https://cli.run.pivotal.io/stable?release=debian64
   sudo dpkg -i cf-cli.deb
   cf login -u $CF_USERNAME -p $CF_PASSWORD -o pivotallabs -s whiteboard -a https://api.run.pivotal.io
+  git status
   git checkout .
   bundle exec rake SPACE=whiteboard cf:deploy:staging
 fi
