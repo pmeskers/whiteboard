2a6492ade3ae128f21e7ffe24605efe691158197
Paul Meskers <paulmeskers@gmail.com>
2016-04-13 16:49:56 -0400

    WIP: First pass at trying to style the emails

--- app/mailers/post_mailer.rb
+++ app/mailers/post_mailer.rb
@@ -4,6 +4,9 @@ class PostMailer < ActionMailer::Base
   def send_to_all(post, to_addresses, from_address, standup_id)
     @post = post
     @standup_id = standup_id
+    @standup = Standup.find(standup_id)
+    @items = post.items_by_type
+    @include_authors = true
     mail  :to => Array(to_addresses),
           :subject => post.title_for_email,
           :from => "#{post.from} <#{from_address}>"
